2c2f848c6e772e2ed525e32db8bc99e1feada375
ruViolence <78062896+ruViolence@users.noreply.github.com>
2024-05-05 18:59:17 +0800

    Optimize zero fee calculation

--- src/main/java/ru/violence/twonyone/util/Utils.java
+++ src/main/java/ru/violence/twonyone/util/Utils.java
@@ -10,6 +10,7 @@ public class Utils {
     public final Random RANDOM = new SecureRandom();
 
     public int calculateAmountWithFee(int amount, double feePercentage) {
+        if (feePercentage == 0) return amount;
         int feeAmount = (int) (amount * feePercentage / 100.0); // Calculate fee amount
         return amount - feeAmount; // Total amount with fee subtracted
     }
